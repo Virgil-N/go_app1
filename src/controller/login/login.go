aac2395c7c2869783a9b329a697a7b65ea18d11b
VirgilN <lieut9011@126.com>
2014-09-01 21:20:07 +0800

    移除数据库内容，现在正常了

--- src/controller/login/login.go
+++ src/controller/login/login.go
@@ -1,9 +1,24 @@
 package login
 
 import (
+	// "fmt"
 	"html/template"
+	"io"
+	// "labix.org/v2/mgo"
+	// "labix.org/v2/mgo/bson"
 	"net/http"
 )
+
+type User struct {
+	Username string
+	Age      string
+	Gender   string
+	Password string
+	Married  string
+	Score    int
+	Likes    []string
+	Friends  []string
+}
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
@@ -11,8 +26,14 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		t, _ := template.ParseFiles("view/page/login.html")
 		t.Execute(w, nil)
 	} else {
-		r.ParseForm()
-		var temp = r.Form["username"][0] + r.Form["password"][0]
-		w.Write([]byte(temp))
+		username := r.FormValue("username")
+		w.Header().Set("Content-Type", "text/plain")
+		//暂时用“tom”，以后换数据库的数据
+		if username == "tom" {
+			io.WriteString(w, "matched")
+		} else {
+			io.WriteString(w, "not matched")
+		}
 	}
+
 }
