8a1d81720a38801916b18a9bb573afa9c6692821
samete <lieut9011@126.com>
2014-09-02 10:08:40 +0800

    搞定cookie

--- src/controller/login/login.go
+++ src/controller/login/login.go
@@ -1,12 +1,13 @@
 package login
 
 import (
-	// "fmt"
+	"fmt"
 	"html/template"
 	"io"
 	// "labix.org/v2/mgo"
 	// "labix.org/v2/mgo/bson"
 	"net/http"
+	"time"
 )
 
 type User struct {
@@ -31,9 +32,19 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		//暂时用“tom”，以后换数据库的数据
 		if username == "tom" && password == "12345" {
-			io.WriteString(w, "matched")
-			//添加cookie
+			//添加cookie(注意添加cookie必须放在服务器返回数据之前)
+			expires := time.Now().AddDate(0, 0, 1)
+			myCookie := http.Cookie{
+				Name:    "testMyCookie",
+				Value:   "hello everyone",
+				Path:    "/login",
+				Expires: expires, //可加可不加？
+				MaxAge:  86400,
+			}
+			http.SetCookie(w, &myCookie)
+			fmt.Println(myCookie)
 
+			io.WriteString(w, "matched")
 		} else {
 			io.WriteString(w, "not matched")
 		}
