5e2720d140fc1bb24de39a2e2d65f045f9ff99bb
VirgilN <lieut9011@126.com>
2014-09-02 21:40:04 +0800

    准备添加页面跳转

--- src/controller/login/login.go
+++ src/controller/login/login.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	// "labix.org/v2/mgo"
-	// "labix.org/v2/mgo/bson"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"net/http"
 	"time"
 )
@@ -31,23 +31,54 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		password := r.FormValue("password")
 		w.Header().Set("Content-Type", "text/plain")
 		//暂时用“tom”，以后换数据库的数据
-		if username == "tom" && password == "12345" {
-			//添加cookie(注意添加cookie必须放在服务器返回数据之前)
+		/*
+			if username == "tom" && password == "12345" {
+				//添加cookie(注意添加cookie必须放在服务器返回数据之前)
+				expires := time.Now().AddDate(0, 0, 1)
+				myCookie := http.Cookie{
+					Name:    "testMyCookie",
+					Value:   "hello everyone",
+					Path:    "/login",
+					Expires: expires, //可加可不加？
+					MaxAge:  86400,
+				}
+				http.SetCookie(w, &myCookie)
+				fmt.Println(myCookie)
+
+				io.WriteString(w, "matched")
+			} else {
+				io.WriteString(w, "not matched")
+			}
+		*/
+		//换成数据库，哈哈
+		session, err := mgo.Dial("localhost:27017")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+
+		session.SetMode(mgo.Monotonic, true)
+		c := session.DB("db1").C("user")
+		var adminUser User
+		c.Find(bson.M{"username": "tom"}).One(&adminUser)
+		if adminUser.Username == username && adminUser.Password == password {
+			//设置cookie
 			expires := time.Now().AddDate(0, 0, 1)
 			myCookie := http.Cookie{
 				Name:    "testMyCookie",
-				Value:   "hello everyone",
-				Path:    "/login",
+				Value:   "success",
+				Path:    "/",     //所有页面都可以读取cookie
 				Expires: expires, //可加可不加？
 				MaxAge:  86400,
 			}
 			http.SetCookie(w, &myCookie)
 			fmt.Println(myCookie)
-
-			io.WriteString(w, "matched")
+			fmt.Println(username, password)
+			io.WriteString(w, "success")
 		} else {
-			io.WriteString(w, "not matched")
+			io.WriteString(w, "fail")
 		}
+
 	}
 
 }
--- src/view/js/login.js
+++ src/view/js/login.js
@@ -1,3 +1,22 @@
 require(["jquery"], function($){
-	
+	$("#login-button").click(function(){
+		var username=$("#username").val();
+		var password=$("#password").val();
+		var formData={
+			username: username,
+			password: password
+		};
+		$.ajax({
+			type: "POST",
+			url: "/login",
+			data: formData,
+			success: function(responseText){
+				if(responseText==="success"){
+					window.location.href="/"
+				}else{
+					alert("用户名或密码错误！")
+				}
+			}
+		});
+	});
 });
